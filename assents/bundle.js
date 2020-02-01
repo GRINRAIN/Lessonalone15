e2a3c21356dfba048524200aedafb2a740c0b436
AllyBrave89 <prosto2tak@ukr.net>
2020-02-01 17:04:18 +0200

    Added files1

---
+++ assents/bundle.js
@@ -0,0 +1,9 @@
+var loginInput = document.querySelector('.js-Login');
+loginInput.addEventListener('change', handlerLoginInputChange);
+function handlerLoginInputChange(event) {
+	console.log('login input was change:', event.target.value);
+}
+loginInput.addEventListener('keydown', handlerLoginInputKeyDown);
+function handlerLoginInputKeyDown (event) {
+	console.log('login input was change:', event.target.value);
+}
